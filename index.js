75f87746b434785920e7c3b04b81cc4a7c808e33
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-09-29 23:42:31 -0400

    reinstalld json

--- index.js
+++ index.js
@@ -1,147 +1,67 @@
-// require modules 
-const fs = require('fs'); 
-const inquirer = require('inquirer'); 
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+var github = require("octonode");
+const sectionPrompts = require("./utils/prompts.js")
+const generateMkdown = require("./utils/generateMarkdown.js");
 
-//linking to page where README is created
-const generatePage = require('./utils/generateMarkdown.js');
+const writeFile = util.promisify(fs.writeFile);
 
-// array of questions for user
-const questions = () => {
-    // using inquirer to prompt questions to user 
-    return inquirer.prompt([
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your GitHub username?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter your GitHub username!');
-                return false; 
-            }
-        } 
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter your email address!');
-                return false; 
-            }
-        }
+// prompt file write location
+let start = async function() { 
+  try { 
+    const { username, password } = await inquirer.prompt([
+        {
+          name: "username",
+          message: "Github Username: ",
+          type: "input",
+        },
+        {
+          name: "password",
+          message: "Github Password: ",
+          type: "password",
+        }])
+          var client = github.client({
+              username: username,
+              password: password,
+            });
+            getAuthentication(client)
+      }
+  catch (err) 
+      {
+      console.log(err);
+      }
+}
 
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is your project name?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter your project name!');
-                return false; 
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please write a short description of your project.',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter a description of your project!');
-                return false; 
-            }
-        }
-    }, 
-    {
-        type: 'list',
-        name: 'license',
-        message: 'What kind of license should your project have?',
-        choices: ['MIT', 'GNU'],
-        default: ["MIT"],
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please choose a license!');
-                return false; 
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'install',
-        message: 'What are the steps required to install your project?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter steps required to install your project!');
-                return false; 
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'How do you use this app?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter a usage description!');
-                return false; 
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'test', 
-        message: 'What command should be run to run tests?',
-        default: 'npm test'
-    },
-    {
-        type: 'input',
-        name: 'contributors',
-        message: 'What does the user need to know about contributing to the repo?'
+// get github authentication
+ function getAuthentication(client) {
+  client.get("/user", {}, function (err, status, body, headers) {
+    if (!err) {
+      authenticated = true;
+    } else {
+      authenticated = false;
+      console.log("Invalid credentials");
     }
-]);
-};
+  });
+}
 
-// function to write README file using file system 
-const writeFile = data => {
-    fs.writeFile('README.md', data, err => {
-        // if there is an error 
-        if (err) {
-            console.log(err);
-            return;
-        // when the README has been created 
-        } else {
-            console.log("Your README has been successfully created!")
-        }
-    })
-}; 
+ 
 
-// function call to initialize program
-questions()
-// getting user answers 
-.then(answers => {
-    return generatePage(answers);
-})
-// using data to display on page 
-.then(data => {
-    return writeFile(data);
-})
-// catching errors 
-.catch(err => {
-    console.log(err)
-})
+  async function init() 
+  {
+    try {
+    await start();
+    const answers = await sectionPrompts();
+    const mkDown = await generateMkdown(answers);
+    await writeFile ("README.md", mkDown);
+    console.log("Success!");
+    }
+    
+      
+        
+    catch (err) {
+      console.log("There was an error writting your file." + err)
+    }
+  }
+  
+  init();
