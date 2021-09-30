26740e921a1b5479beeeb32faef0c25d19d40575
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-09-30 10:41:14 -0400

    edited index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const inquirer = require('inquirer'); 
 
 // linking to page where the README is developed 
-const generatePage = require('./utils/generateMarkdown.js');
+const generatePage = require('');
 
 // array featuring questions for user
 const questions = () => {
